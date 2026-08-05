453960e0c897eff0207cdfac70bca054582807d4
Madhu Vayala <madhuvayala.off@gmail.com>
2026-08-05 16:56:58 +0530

    new changes

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Userservice } from '../userservice';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +8,34 @@ import { Component } from '@angular/core';
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {}
+export class Login {
+  studentName$=new Subject();
+  rollNumber$=new Subject<Number>();//this is for number type onlu it will not accept any other tipe as a value 
+  takeUntil=new Subject<void>();//when you dont want any value for subject ew go with void
+  // madhuSub$=new Subject('madhu'); when you create a subject we cannot innitialize with defolt value it will give an error same thing with behaviorSubject we ca do it will not give any error
+  
+//for providing the data type for subject
+courceName$:Subject<string>=new Subject<string>();
+userSrv=inject(Userservice);
+
+  constructor(){
+    setTimeout(()=>{
+  this.studentName$.next('angular22');
+  this.rollNumber$.next(8309650694);
+  this.takeUntil.next();
+  this.userSrv.courceDuration$.next('2+1 =3 months');
+
+    },4000);
+  }
+
+  ngOnInit(){
+    this.studentName$.subscribe((rea:any)=>{
+      // debugger;
+      console.log(this.studentName$);
+      //bihavior Subject
+      this.userSrv.courceDuration$.subscribe((data:any)=>{
+        debugger;
+      })
+    })
+  }
+}
---
+++ src/app/userservice.ts
@@ -0,0 +1,13 @@
+import { Service } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Service()
+export class Userservice {
+    courceDuration$=new BehaviorSubject('2 months');//when we creating behaviorSubject we are initializing a value
+
+    //if when you emit the value in subject if the component whereevar we are subscribing loaded then only will be able to recive the value if the component will be load after some time we wont get the updated value 
+    //But incase of behavior  subjet it track the value of data and it will give the exact result load the componet or not it will give the exact data 
+    constructor(){
+        // this.courceDuration$.next('3 months need')
+    }
+}
